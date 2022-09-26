1d9bb1b9a0fec63cff99c8255e8111a85c89e131
Stanislav Filipov <stanfilip0v@gmail.com>
2022-09-26 16:40:10 +0300

    feat: updating the farm module with authorization and database interaction

--- src/farm/dto/update-farm.dto.ts
+++ src/farm/dto/update-farm.dto.ts
@@ -1,1 +1,9 @@
-export class UpdateFarmDto {}
+export class UpdateFarmDto {
+  name: string;
+  sub_account_name: string;
+  location: string;
+  initial_hash_rate: number;
+  btc_wallet: string;
+  default_btc_payout_address: string;
+  maintanance_fee: number;
+}
--- src/farm/farm.controller.ts
+++ src/farm/farm.controller.ts
@@ -1,32 +1,53 @@
-import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import RoleGuard from '../auth/guards/role.guard';
+import { Role } from '../user/roles';
 import { CreateFarmDto } from './dto/create-farm.dto';
 import { UpdateFarmDto } from './dto/update-farm.dto';
+import { Farm } from './farm.model';
 import { FarmService } from './farm.service';
+import { IsOwnerGuard } from './guards/is-owner.guard';
 
 @Controller('farm')
 export class FarmController {
   constructor(private farmService: FarmService) {}
 
   @Get()
-  async findAll(): Promise<any[]> {
+  async findAll(): Promise<Farm[]> {
     return this.farmService.findAll();
   }
 
   @Get(':id')
-  async findOne(): Promise<any> {
-    return this.farmService.findOne();
+  async findOne(@Param('id', ParseIntPipe) id: number): Promise<Farm> {
+    return this.farmService.findOne(id);
   }
 
+  @UseGuards(RoleGuard([Role.FARM_ADMIN]))
   @Post()
-  async create(@Body() createFarmDto: CreateFarmDto): Promise<void> {
-    return this.farmService.createOne();
+  async create(
+    @Request() req,
+    @Body() createFarmDto: CreateFarmDto,
+  ): Promise<Farm> {
+    const farm = await this.farmService.createOne(createFarmDto, req.user.id);
+
+    return farm;
   }
 
+  @UseGuards(RoleGuard([Role.FARM_ADMIN, Role.SUPER_ADMIN]), IsOwnerGuard)
   @Put(':id')
   async update(
-    @Param('id') id: string,
+    @Param('id', ParseIntPipe) id: number,
     @Body() updateFarmDto: UpdateFarmDto,
-  ): Promise<void> {
-    return this.farmService.updateOne();
+  ): Promise<string> {
+    return 'This is accesable';
   }
 }
