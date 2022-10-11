1cd1a1ffc8c33eb9d8d922fb7139b5b61067a5d8
Stanislav Filipov <stanfilip0v@gmail.com>
2022-10-11 16:00:40 +0300

    feat: fetching minted nft by given tx hash

--- src/graphql/graphql.service.ts
+++ src/graphql/graphql.service.ts
@@ -1,13 +1,16 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { print } from 'graphql';
+import { AxiosResponse } from 'axios';
 import {
   MarketplaceNftQuery,
   MarketplaceNftDocument,
   MarketplaceCollectionQuery,
   MarketplaceCollectionDocument,
+  GetNftByTxHashQuery,
+  GetNftByTxHashDocument,
 } from './types';
-import { print } from 'graphql';
-import { MarketplaceNftFilters } from 'src/nft/utils';
+import { MarketplaceNftFilters } from '../nft/utils';
 
 @Injectable()
 export class GraphqlService {
@@ -21,6 +24,24 @@ export class GraphqlService {
     });
 
     return res.data;
+  }
+
+  async getMintedNftUuid(tx_hash: string): Promise<{ uuid: string }> {
+    const res: AxiosResponse<{ data: GetNftByTxHashQuery }> =
+      await this.httpService.axiosRef.post(process.env.GRAPHQL_URL, {
+        query: print(GetNftByTxHashDocument),
+        variables: { tx_hash },
+      });
+
+    if (!res.data.data.nft_nft.length) {
+      throw new NotFoundException();
+    }
+
+    const [nft] = res.data.data.nft_nft;
+
+    const memo = JSON.parse(nft.transaction.memo);
+
+    return { uuid: memo.uuid };
   }
 
   async fetchCollection(): Promise<MarketplaceCollectionQuery> {
