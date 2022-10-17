372fabaa2a80e8dc35139d962f1161dcd6668385
Valentin Staykov <v.staykov@gmail.com>
2022-10-17 11:21:24 +0000

    started adding explore farms page

--- apps/frontend/src/features/mining-farm/presentation/pages/ExploreFarmsPage.tsx
+++ apps/frontend/src/features/mining-farm/presentation/pages/ExploreFarmsPage.tsx
@@ -1,95 +1,55 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
-import '../styles/page-farm-view-component.css';
-import ProfileHeader from '../../../collection/presentation/components/ProfileHeader';
-import Breadcrumbs from '../../../../core/presentation/components/Breadcrumbs';
-import FarmViewPageStore from '../stores/FarmViewPageStore';
+import '../styles/page-explore-farms-component.css';
 import AppStore from '../../../../core/presentation/stores/AppStore';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import PageLayoutComponent from '../../../../core/presentation/components/PageLayoutComponent';
 import { inject, observer } from 'mobx-react';
 import PageHeader from '../../../header/presentation/components/PageHeader';
 import PageFooter from '../../../footer/presentation/components/PageFooter';
-import LoadingIndicator from '../../../../core/presentation/components/LoadingIndicator';
-import AppRoutes from '../../../app-routes/entities/AppRoutes';
-import CollectionPreviewsGrid from '../../../collection/presentation/components/CollectionPreviewsGrid';
+import Input, { InputType } from 'apps/frontend/src/core/presentation/components/Input';
+import InputAdornment from '@mui/material/InputAdornment';
+import Svg from 'apps/frontend/src/core/presentation/components/Svg';
+import SearchIcon from '@mui/icons-material/Search';
 
 interface Props {
     appStore?: AppStore
-    farmViewPageStore?: FarmViewPageStore,
 }
 
-function FarmViewPage({ appStore, farmViewPageStore }: Props) {
+function ExploreFarmsPage({ appStore }: Props) {
 
-    const { farmId } = useParams();
-    const navigate = useNavigate();
+    const [searchString, setSearchString] = useState('');
 
     useEffect(() => {
         appStore.useLoading(async () => {
-            await farmViewPageStore.init(farmId);
+            await exploreFarmsPageStore.init();
         });
     }, []);
 
-    const farm = farmViewPageStore.farmProfile;
-
-    const crumbs = [
-        { name: 'Marketplace', onClick: () => { navigate(AppRoutes.MARKETPLACE) } },
-        { name: 'Explore Farms', onClick: () => { navigate(AppRoutes.EXPLORE_FARMS) } },
-        { name: `Farm Owner: ${farm ? farm.name : ''}`, onClick: () => {} },
-    ]
-
     return (
         <PageLayoutComponent
-            className = { 'PageFarmView' } >
+            className = { 'PageExploreFarms' } >
             <PageHeader />
-
-            { farm === null && (
-                <LoadingIndicator />
-            ) }
-
-            { farm !== null && (
-                <div className={'PageContent AppContent'} >
-                    <Breadcrumbs crumbs={crumbs}/>
-                    <ProfileHeader coverPictureUrl={farm.coverImgUrl} profilePictureUrl={farm.profileImgurl} />
-                    <div className={'Heading2'}>{farm.name}</div>
-                    <div className={'Grid GridColumns2'}>
-                        <div className={'FarmDescription'}>{farm.description}</div>
-                        <div className={'BorderContainer'}>
-                            <div className={'FlexRow FarmInfoRow'}>
-                                <div className={'FarmInfoLabel'}>Total Hashrate</div>
-                                <div className={'FarmInfoValue'}>102.000 EH/s</div>
-                            </div>
-                            <div className={'FlexRow FarmInfoRow'}>
-                                <div className={'FarmInfoLabel'}>Hashrate (1h average)</div>
-                                <div className={'FarmInfoValue'}>80.345 EH/s</div>
-                            </div>
-                            <div className={'FlexRow FarmInfoRow'}>
-                                <div className={'FarmInfoLabel'}>Active Workers</div>
-                                <div className={'FarmInfoValue'}>1023</div>
-                            </div>
-                            <div className={'FlexRow FarmInfoRow'}>
-                                <div className={'FarmInfoLabel'}>Collections Owned</div>
-                                <div className={'FarmInfoValue'}>2</div>
-                            </div>
-                            <div className={'FlexRow FarmInfoRow'}>
-                                <div className={'FarmInfoLabel'}>NFTs Owned</div>
-                                <div className={'FarmInfoValue'}>1400</div>
-                            </div>
-                            <div className={'FlexRow FarmInfoRow'}>
-                                <div className={'FarmInfoLabel'}>Total NFTs Sold</div>
-                                <div className={'FarmInfoValue'}>735</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={'H2'}>Collections Owned</div>
-                    <CollectionPreviewsGrid collectionPreviewsGridState={farmViewPageStore.collectionPreviewsGridState}/>
-                </div>
-            ) }
-
+            <div className={'PageContent AppContent FlexColumn'} >
+                <div className={'H2 Bold'}>Explore Farms</div>
+                <Input
+                    inputType={InputType.TEXT}
+                    className={'SearchBar'}
+                    value = {searchString}
+                    onChange = { setSearchString }
+                    placeholder = {'Search Collections name'}
+                    InputProps={{
+                        startAdornment: <InputAdornment position="start" >
+                            <Svg svg={SearchIcon} />
+                        </InputAdornment>,
+                    }}
+                />
+                <FarmPreviewsGrid farmPreviewGridState={farmViewPageStore.collectionPreviewsGridState}/>
+            </div>
             <PageFooter />
         </PageLayoutComponent>
     )
 
 }
 
-export default inject((stores) => stores)(observer(FarmViewPage));
+export default inject((stores) => stores)(observer(ExploreFarmsPage));
